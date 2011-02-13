370106ef50295a7869e88c5eb405e1df160bba83
Wilfred Springer <wilfred@flotsam.nl>
2011-02-13 15:52:16 +0100

    Added an example of var length encoded content, just to answer StackOverflow question.

---
+++ preon-samples/preon-sample-varlength-encoding/src/test/java/org/codehaus/preon/sample/varlength/VariableLengthByteArrayCodecTest.java
@@ -0,0 +1,8 @@
+package org.codehaus.preon.sample.varlength;
+
+/**
+ * Created by IntelliJ IDEA. User: wilfred Date: 2/13/11 Time: 2:53 PM To change this template use File | Settings |
+ * File Templates.
+ */
+public class VariableLengthByteArrayCodecTest {
+}
